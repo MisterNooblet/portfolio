87804b85b6e69dc58fd7fe125f7d9b8c837f01c7
misternooblet <dragonroadsupport@gmail.com>
2023-03-18 15:08:40 +0200

    Basic folder structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { About, Contact, Error404, Hero, Hobbies, Layout, Portfolio, Resume } from "./pages";
+
+
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      errorElement: <Error404 />,
+      children: [
+        { path: '/', element: <Hero /> },
+        { path: '/about', element: <About /> },
+        { path: '/contact', element: <Contact /> },
+        { path: '/hobbies', element: <Hobbies /> },
+        { path: '/portfolio', element: <Portfolio /> },
+        { path: '/resume', element: <Resume /> },
+
+      ]
+    }
+
+  ])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import About from "./About/About.page";
+import Contact from "./Contact/Contact.page";
+import Hero from "./Hero/Hero.page";
+import Hobbies from "./Hobbies/Hobbies.page";
+import Layout from "./Layout/Layout";
+import Portfolio from "./Portfolio/Portfolio.page";
+import Resume from "./Resume/Resume.page";
+import Error404 from "./Error/Error404.page";
+
+export { About, Error404, Contact, Hero, Hobbies, Layout, Portfolio, Resume }
