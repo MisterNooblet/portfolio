5eda2c852a2f5d7d40c5049089bcc83aa8c87010
misternooblet <dragonroadsupport@gmail.com>
2023-03-18 15:08:30 +0200

    Basic folder structure

---
+++ src/pages/Layout/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <header>
+      <main>
+        <nav></nav>
+      </main>
+    </header>
+  );
+};
+
+export default Navbar;
